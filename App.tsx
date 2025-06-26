e8c2a235808f583624b985d8d8f1607daa05c9ce
azedine ouhadou <xperazazedine@gmail.com>
2025-06-26 16:26:23 +0100

    move files into root directory

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
+      {/* <Button>click me</Button> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBlockColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+
+  itemText: {
+    fontSize: 18,
+    fontWeight: "200",
   },
 });
