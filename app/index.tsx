5617bf5e13d056b9d4d98beb031b6b64cf595c5c
azedine ouhadou <xperazazedine@gmail.com>
2025-07-14 23:46:37 +0100

    implement shopping list functionality with input handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 import { ShoppingListItem } from "../components/ShopingListItem";
+import { theme } from "../theme";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted: boolean;
+};
 
 export default function App() {
+  const [ShoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+  const [value, setValue] = useState("");
+
+  const handleSubmit = () => {
+    if (value) {
+      setShoppingList([
+        ...ShoppingList,
+        {
+          id: new Date().toISOString(),
+          name: value,
+          isCompleted: false,
+        },
+      ]);
+      setValue("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Tea" isCompleted />
-      <ShoppingListItem name="Juice" />
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={(value) => setValue(value)}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+
+      {ShoppingList.map((item) => (
+        <ShoppingListItem
+          key={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+        />
+      ))}
 
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -18,6 +59,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+    height: "auto",
+  },
+  contentContainer: {
+    padding: 12,
+  },
+  textInput: {
+    borderColor: theme.colorGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 12,
+    borderRadius: 50,
+    justifyContent: "flex-end",
+    backgroundColor: theme.colorWhite,
   },
 });
