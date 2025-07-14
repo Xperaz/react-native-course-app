b2b5ec536095f8cc142e39c5567681abe89dea70
azedine ouhadou <xperazazedine@gmail.com>
2025-07-14 14:56:44 +0100

    add stacked navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,45 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import Foundation from "@expo/vector-icons/Foundation";
+import { theme } from "../theme";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.colorCerulean,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => {
+            return (
+              <Fontisto name="nav-icon-list-a" size={size} color={color} />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome5 name="stopwatch" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => {
+            return <Foundation name="lightbulb" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { ShoppingListItem } from "../components/ShopingListItem";
-import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{
-          textAlign: "center",
-          marginBottom: 18,
-        }}
-      >
-        Go to counter
-      </Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Juice" />
